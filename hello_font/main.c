f039751589edbc557be7a0a684acdebfcaae986e
MikeH <matroxmike@icloud.com>
2016-11-28 23:50:54 +1100

    hello_font works with vg library - default_font set in library

--- hello_font/main.c
+++ hello_font/main.c
@@ -38,101 +38,117 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 static const char *strnchr(const char *str, size_t len, char c)
 {
-   const char *e = str + len;
-   do {
-      if (*str == c) {
-         return str;
-      }
-   } while (++str < e);
-   return NULL;
+	const char *e = str + len;
+	do {
+		if (*str == c) {
+			return str;
+		}
+	} while (++str < e);
+	return NULL;
 }
 
-int32_t render_subtitle(GRAPHICS_RESOURCE_HANDLE img, const char *text, const int skip, const uint32_t text_size, const uint32_t y_offset)
+int32_t render_subtitle(GRAPHICS_RESOURCE_HANDLE img, const char *text,
+			const int skip, const uint32_t text_size,
+			const uint32_t y_offset)
 {
-   uint32_t text_length = strlen(text)-skip;
-   uint32_t width=0, height=0;
-   const char *split = text;
-   int32_t s=0;
-   int len = 0; // length of pre-subtitle
-   uint32_t img_w, img_h;
+	uint32_t text_length = strlen(text) - skip;
+	uint32_t width = 0, height = 0;
+	const char *split = text;
+	int32_t s = 0;
+	int len = 0;		// length of pre-subtitle
+	uint32_t img_w, img_h;
 
-   graphics_get_resource_size(img, &img_w, &img_h);
+	graphics_get_resource_size(img, &img_w, &img_h);
 
-   if (text_length==0)
-      return 0;
-   while (split[0]) {
-      s = graphics_resource_text_dimensions_ext(img, split, text_length-(split-text), &width, &height, text_size);
-      if (s != 0) return s;
-      if (width > img_w) {
-         const char *space = strnchr(split, text_length-(split-text), ' ');
-         if (!space) {
-            len = split+1-text;
-            split = split+1;
-         } else {
-            len = space-text;
-            split = space+1;
-         }
-      } else {
-         break;
-      }
-   }
-   // split now points to last line of text. split-text = length of initial text. text_length-(split-text) is length of last line
-   if (width) {
-      s = graphics_resource_render_text_ext(img, (img_w - width)>>1, y_offset-height,
-                                     GRAPHICS_RESOURCE_WIDTH,
-                                     GRAPHICS_RESOURCE_HEIGHT,
-                                     GRAPHICS_RGBA32(0xff,0xff,0xff,0xff), /* fg */
-                                     GRAPHICS_RGBA32(0,0,0,0x80), /* bg */
-                                     split, text_length-(split-text), text_size);
-      if (s!=0) return s;
-   }
-   return render_subtitle(img, text, skip+text_length-len, text_size, y_offset - height);
+	if (text_length == 0)
+		return 0;
+	while (split[0]) {
+		s = graphics_resource_text_dimensions_ext(img, split,
+							  text_length - (split -
+									 text),
+							  &width, &height,
+							  text_size);
+		if (s != 0)
+			return s;
+		if (width > img_w) {
+			const char *space =
+			    strnchr(split, text_length - (split - text), ' ');
+			if (!space) {
+				len = split + 1 - text;
+				split = split + 1;
+			} else {
+				len = space - text;
+				split = space + 1;
+			}
+		} else {
+			break;
+		}
+	}
+	// split now points to last line of text. split-text = length of initial text. text_length-(split-text) is length of last line
+	if (width) {
+		s = graphics_resource_render_text_ext(img, (img_w - width) >> 1, y_offset - height, GRAPHICS_RESOURCE_WIDTH, GRAPHICS_RESOURCE_HEIGHT, GRAPHICS_RGBA32(0xff, 0xff, 0xff, 0xff),	/* fg */
+						      GRAPHICS_RGBA32(0, 0, 0, 0x80),	/* bg */
+						      split,
+						      text_length - (split -
+								     text),
+						      text_size);
+		if (s != 0)
+			return s;
+	}
+	return render_subtitle(img, text, skip + text_length - len, text_size,
+			       y_offset - height);
 }
 
 int main(void)
 {
-   GRAPHICS_RESOURCE_HANDLE img;
-   uint32_t width, height;
-   int LAYER=1;
-   bcm_host_init();
-   int s;
+	GRAPHICS_RESOURCE_HANDLE img;
+	uint32_t width, height;
+	int LAYER = 1;
+	bcm_host_init();
+	int s;
 
-   s = gx_graphics_init(".");
-   assert(s == 0);
+	s = gx_graphics_init(".");
+	assert(s == 0);
 
-   s = graphics_get_display_size(0, &width, &height);
-   assert(s == 0);
+	s = graphics_get_display_size(0, &width, &height);
+	assert(s == 0);
 
-   s = gx_create_window(0, width, height, GRAPHICS_RESOURCE_RGBA32, &img);
-   assert(s == 0);
+	s = gx_create_window(0, width, height, GRAPHICS_RESOURCE_RGBA32, &img);
+	assert(s == 0);
 
-   // transparent before display to avoid screen flash
-   graphics_resource_fill(img, 0, 0, width, height, GRAPHICS_RGBA32(0,0,0,0x00));
+	// transparent before display to avoid screen flash
+	graphics_resource_fill(img, 0, 0, width, height,
+			       GRAPHICS_RGBA32(0, 0, 0, 0x00));
 
-   graphics_display_resource(img, 0, LAYER, 0, 0, GRAPHICS_RESOURCE_WIDTH, GRAPHICS_RESOURCE_HEIGHT, VC_DISPMAN_ROT0, 1);
+	graphics_display_resource(img, 0, LAYER, 0, 0, GRAPHICS_RESOURCE_WIDTH,
+				  GRAPHICS_RESOURCE_HEIGHT, VC_DISPMAN_ROT0, 1);
 
-   uint32_t text_size = 10;
-   while (1) {
-      const char *text = "The quick brown fox jumps over the lazy dog";
-      uint32_t y_offset = height-60+text_size/2;
-      graphics_resource_fill(img, 0, 0, width, height, GRAPHICS_RGBA32(0,0,0,0x00));
-      // blue, at the top (y=40)
-      graphics_resource_fill(img, 0, 40, width, 1, GRAPHICS_RGBA32(0,0,0xff,0xff));
+	uint32_t text_size = 10;
+	while (1) {
+		const char *text =
+		    "The quick brown fox jumps over the lazy dog";
+		uint32_t y_offset = height - 60 + text_size / 2;
+		graphics_resource_fill(img, 0, 0, width, height,
+				       GRAPHICS_RGBA32(0, 0, 0, 0x00));
+		// blue, at the top (y=40)
+		graphics_resource_fill(img, 0, 40, width, 1,
+				       GRAPHICS_RGBA32(0, 0, 0xff, 0xff));
 
-      // green, at the bottom (y=height-40)
-      graphics_resource_fill(img, 0, height-40, width, 1, GRAPHICS_RGBA32(0,0xff,0,0xff));
+		// green, at the bottom (y=height-40)
+		graphics_resource_fill(img, 0, height - 40, width, 1,
+				       GRAPHICS_RGBA32(0, 0xff, 0, 0xff));
 
-      // draw the subtitle text
-      render_subtitle(img, text, 0, text_size,  y_offset);
-      graphics_update_displayed_resource(img, 0, 0, 0, 0);
-      text_size += 1;
-      if (text_size > 50)
-         text_size = 10;
-   }
+		// draw the subtitle text
+		render_subtitle(img, text, 0, text_size, y_offset);
+		graphics_update_displayed_resource(img, 0, 0, 0, 0);
+		text_size += 1;
+		if (text_size > 50)
+			text_size = 10;
+	}
 
-   graphics_display_resource(img, 0, LAYER, 0, 0, GRAPHICS_RESOURCE_WIDTH, GRAPHICS_RESOURCE_HEIGHT, VC_DISPMAN_ROT0, 0);
-   graphics_delete_resource(img);
+	graphics_display_resource(img, 0, LAYER, 0, 0, GRAPHICS_RESOURCE_WIDTH,
+				  GRAPHICS_RESOURCE_HEIGHT, VC_DISPMAN_ROT0, 0);
+	graphics_delete_resource(img);
 
-   return 0;
+	return 0;
 }
-
--- libs/vgfont/font.c
+++ libs/vgfont/font.c
@@ -53,27 +53,26 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <unistd.h>
 #include <sys/types.h>
 
-#include "graphics_x_private.h"
-#include "vgft.h"
+#include "../libs/vgfont/graphics_x_private.h"
+#include "../libs/vgfont/vgft.h"
 
 #define VMCS_INSTALL_PREFIX ""
 
 /** The one and only (default) font we support for now.
   */
-static struct
-{
-   const char *file;
-   void *mem;
-   size_t len;
-} default_font = { "Vera.ttf" };
+static struct {
+	const char *file;
+	void *mem;
+	size_t len;
+} default_font = {
+"Vera.ttf"};
 
 /** An entry in our list of fonts
   */
-typedef struct gx_font_cache_entry_t
-{
-   struct gx_font_cache_entry_t *next;
-   VGFT_FONT_T font;
-   uint32_t ptsize;                    /** size in points, 26.6 */
+typedef struct gx_font_cache_entry_t {
+	struct gx_font_cache_entry_t *next;
+	VGFT_FONT_T font;
+	uint32_t ptsize;	       /** size in points, 26.6 */
 } gx_font_cache_entry_t;
 
 static char fname[128];
@@ -84,68 +83,65 @@ static VGFT_FONT_T *find_font(const char *text, uint32_t text_size);
 
 VCOS_STATUS_T gx_priv_font_init(const char *font_dir)
 {
-   VCOS_STATUS_T ret;
-   size_t len;
-   int rc;
-   if (vgft_init())
-   {
-      ret = VCOS_ENOMEM;
-      goto fail_init;
-   }
+	VCOS_STATUS_T ret;
+	size_t len;
+	int rc;
+	if (vgft_init()) {
+		ret = VCOS_ENOMEM;
+		goto fail_init;
+	}
 
-   int fd = -1;
-   // search for the font
-   sprintf(fname, "%s/%s", font_dir, default_font.file);
-   fd = open(fname, O_RDONLY);
+	int fd = -1;
+	// search for the font
+	sprintf(fname, "%s/%s", font_dir, default_font.file);
+	fd = open(fname, O_RDONLY);
 
-   if (fd < 0)
-   {
-      GX_ERROR("Could not open font file '%s'", default_font.file);
-      ret = VCOS_ENOENT;
-      goto fail_open;
-   }
+	if (fd < 0) {
+		GX_ERROR("Could not open font file '%s'", default_font.file);
+		ret = VCOS_ENOENT;
+		goto fail_open;
+	}
 
-   len = lseek(fd, 0, SEEK_END);
-   lseek(fd, 0, SEEK_SET);
+	len = lseek(fd, 0, SEEK_END);
+	lseek(fd, 0, SEEK_SET);
 
-   default_font.mem = vcos_malloc(len, default_font.file);
-   if (!default_font.mem)
-   {
-      GX_ERROR("No memory for font %s", fname);
-      ret = VCOS_ENOMEM;
-      goto fail_mem;
-   }
+	default_font.mem = vcos_malloc(len, default_font.file);
+	if (!default_font.mem) {
+		GX_ERROR("No memory for font %s", fname);
+		ret = VCOS_ENOMEM;
+		goto fail_mem;
+	}
 
-   rc = read(fd, default_font.mem, len);
-   if (rc != len)
-   {
-      GX_ERROR("Could not read font %s", fname);
-      ret = VCOS_EINVAL;
-      goto fail_rd;
-   }
-   default_font.len = len;
-   close(fd);
+	rc = read(fd, default_font.mem, len);
+	if (rc != len) {
+		GX_ERROR("Could not read font %s", fname);
+		ret = VCOS_EINVAL;
+		goto fail_rd;
+	}
+	default_font.len = len;
+	close(fd);
 
-   GX_TRACE("Opened font file '%s'", fname);
+	GX_TRACE("Opened font file '%s'", fname);
 
-   inited = 1;
-   return VCOS_SUCCESS;
+	inited = 1;
+	return VCOS_SUCCESS;
 
-fail_rd:
-   vcos_free(default_font.mem);
-fail_mem:
-   if (fd >= 0) close(fd);
-fail_open:
-   vgft_term();
-fail_init:
-   return ret;
+ fail_rd:
+	vcos_free(default_font.mem);
+ fail_mem:
+	if (fd >= 0)
+		close(fd);
+ fail_open:
+	vgft_term();
+ fail_init:
+	return ret;
 }
 
 void gx_priv_font_term(void)
 {
-   gx_font_cache_flush();
-   vgft_term();
-   vcos_free(default_font.mem);
+	gx_font_cache_flush();
+	vgft_term();
+	vcos_free(default_font.mem);
 }
 
 /** Render text.
@@ -154,109 +150,100 @@ void gx_priv_font_term(void)
   * FIXME: Not UTF-8 aware
   * FIXME: better caching
   */
-VCOS_STATUS_T gx_priv_render_text( GX_DISPLAY_T *disp,
-                                   GRAPHICS_RESOURCE_HANDLE res,
-                                   int32_t x,
-                                   int32_t y,
-                                   uint32_t width,
-                                   uint32_t height,
-                                   uint32_t fg_colour,
-                                   uint32_t bg_colour,
-                                   const char *text,
-                                   uint32_t text_length,
-                                   uint32_t text_size )
+VCOS_STATUS_T gx_priv_render_text(GX_DISPLAY_T * disp,
+				  GRAPHICS_RESOURCE_HANDLE res,
+				  int32_t x,
+				  int32_t y,
+				  uint32_t width,
+				  uint32_t height,
+				  uint32_t fg_colour,
+				  uint32_t bg_colour,
+				  const char *text,
+				  uint32_t text_length, uint32_t text_size)
 {
-   VGfloat vg_colour[4];
-   VGFT_FONT_T *font;
-   VGPaint fg;
-   GX_CLIENT_STATE_T save;
-   VCOS_STATUS_T status = VCOS_SUCCESS;
-   int clip = 1;
+	VGfloat vg_colour[4];
+	VGFT_FONT_T *font;
+	VGPaint fg;
+	GX_CLIENT_STATE_T save;
+	VCOS_STATUS_T status = VCOS_SUCCESS;
+	int clip = 1;
 
-   vcos_demand(inited); // has gx_font_init() been called?
+	vcos_demand(inited);	// has gx_font_init() been called?
 
-   gx_priv_save(&save, res);
+	gx_priv_save(&save, res);
 
-   if (width == GRAPHICS_RESOURCE_WIDTH &&
-       height == GRAPHICS_RESOURCE_HEIGHT)
-   {
-      clip = 0;
-   }
+	if (width == GRAPHICS_RESOURCE_WIDTH &&
+	    height == GRAPHICS_RESOURCE_HEIGHT) {
+		clip = 0;
+	}
 
-   width = (width == GRAPHICS_RESOURCE_WIDTH) ? res->width : width;
-   height = (height == GRAPHICS_RESOURCE_HEIGHT) ? res->height : height;
-   font = find_font(text, text_size);
-   if (!font)
-   {
-      status = VCOS_ENOMEM;
-      goto finish;
-   }
+	width = (width == GRAPHICS_RESOURCE_WIDTH) ? res->width : width;
+	height = (height == GRAPHICS_RESOURCE_HEIGHT) ? res->height : height;
+	font = find_font(text, text_size);
+	if (!font) {
+		status = VCOS_ENOMEM;
+		goto finish;
+	}
+	// setup the clipping rectangle
+	if (clip) {
+		VGint coords[] = { x, y, width, height };
+		vgSeti(VG_SCISSORING, VG_TRUE);
+		vgSetiv(VG_SCISSOR_RECTS, 4, coords);
+	}
+	// setup the background colour if needed
+	if (bg_colour != GRAPHICS_TRANSPARENT_COLOUR) {
+		int err;
+		VGfloat rendered_w, rendered_h;
+		VGfloat vg_bg_colour[4];
 
-   // setup the clipping rectangle
-   if (clip)
-   {
-      VGint coords[] = {x,y,width,height};
-      vgSeti(VG_SCISSORING, VG_TRUE);
-      vgSetiv(VG_SCISSOR_RECTS, 4, coords);
-   }
+		// setup the background colour...
+		gx_priv_colour_to_paint(bg_colour, vg_bg_colour);
+		vgSetfv(VG_CLEAR_COLOR, 4, vg_bg_colour);
 
-   // setup the background colour if needed
-   if (bg_colour != GRAPHICS_TRANSPARENT_COLOUR)
-   {
-      int err;
-      VGfloat rendered_w, rendered_h;
-      VGfloat vg_bg_colour[4];
+		// fill in a rectangle...
+		vgft_get_text_extents(font, text, text_length, (VGfloat) x,
+				      (VGfloat) y, &rendered_w, &rendered_h);
 
-      // setup the background colour...
-      gx_priv_colour_to_paint(bg_colour, vg_bg_colour);
-      vgSetfv(VG_CLEAR_COLOR, 4, vg_bg_colour);
+		if ((0 < (VGint) rendered_w) && (0 < (VGint) rendered_h)) {
+			// Have to compensate for the messed up y position of multiline text.
+			VGfloat offset = vgft_first_line_y_offset(font);
+			int32_t bottom = y + offset - rendered_h;
 
-      // fill in a rectangle...
-      vgft_get_text_extents(font, text, text_length, (VGfloat)x, (VGfloat)y, &rendered_w, &rendered_h);
+			vgClear(x, bottom, (VGint) rendered_w,
+				(VGint) rendered_h);
+			err = vgGetError();
+			if (err) {
+				GX_LOG("Error %d clearing bg text %d %d %g %g",
+				       err, x, y, rendered_w, rendered_h);
+				vcos_assert(0);
+			}	// if
+		}		// if
+	}			// if
+	// setup the foreground colour
+	fg = vgCreatePaint();
+	if (!fg) {
+		status = VCOS_ENOMEM;
+		goto finish;
+	}
+	// draw the foreground text
+	vgSetParameteri(fg, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
+	gx_priv_colour_to_paint(fg_colour, vg_colour);
+	vgSetParameterfv(fg, VG_PAINT_COLOR, 4, vg_colour);
+	vgSetPaint(fg, VG_FILL_PATH);
 
-      if ( ( 0 < (VGint)rendered_w ) && ( 0 < (VGint)rendered_h ) )
-      {
-         // Have to compensate for the messed up y position of multiline text.
-         VGfloat offset = vgft_first_line_y_offset(font);
-         int32_t bottom = y + offset - rendered_h;
+	vgft_font_draw(font, (VGfloat) x, (VGfloat) y, text, text_length,
+		       VG_FILL_PATH);
 
-         vgClear(x, bottom, (VGint)rendered_w, (VGint)rendered_h);
-         err = vgGetError();
-         if (err)
-         {
-            GX_LOG("Error %d clearing bg text %d %d %g %g",
-                   err, x, y, rendered_w, rendered_h);
-            vcos_assert(0);
-         } // if
-      } // if
-   } // if
-   // setup the foreground colour
-   fg = vgCreatePaint();
-   if (!fg)
-   {
-      status = VCOS_ENOMEM;
-      goto finish;
-   }
+	vgDestroyPaint(fg);
 
-   // draw the foreground text
-   vgSetParameteri(fg, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
-   gx_priv_colour_to_paint(fg_colour, vg_colour);
-   vgSetParameterfv(fg, VG_PAINT_COLOR, 4, vg_colour);
-   vgSetPaint(fg, VG_FILL_PATH);
+	vcos_assert(vgGetError() == 0);
+	vgSeti(VG_SCISSORING, VG_FALSE);
 
-   vgft_font_draw(font, (VGfloat)x, (VGfloat)y, text, text_length, VG_FILL_PATH);
+ finish:
+	gx_priv_restore(&save);
 
-   vgDestroyPaint(fg);
-
-   vcos_assert(vgGetError() == 0);
-   vgSeti(VG_SCISSORING, VG_FALSE);
-
-finish:
-   gx_priv_restore(&save);
-
-   return status;
+	return status;
 }
-
 
 /** Find a font in our cache, or create a new entry in the cache.
   *
@@ -264,92 +251,85 @@ finish:
   */
 static VGFT_FONT_T *find_font(const char *text, uint32_t text_size)
 {
-   int ptsize, dpi_x = 0, dpi_y = 0;
-   VCOS_STATUS_T status;
-   gx_font_cache_entry_t *font;
+	int ptsize, dpi_x = 0, dpi_y = 0;
+	VCOS_STATUS_T status;
+	gx_font_cache_entry_t *font;
 
-   ptsize = text_size << 6; // freetype takes size in points, in 26.6 format.
+	ptsize = text_size << 6;	// freetype takes size in points, in 26.6 format.
 
-   for (font = fonts; font; font = font->next)
-   {
-      if (font->ptsize == ptsize)
-         return &font->font;
-   }
+	for (font = fonts; font; font = font->next) {
+		if (font->ptsize == ptsize)
+			return &font->font;
+	}
 
-   font = vcos_malloc(sizeof(*font), "font");
-   if (!font)
-      return NULL;
+	font = vcos_malloc(sizeof(*font), "font");
+	if (!font)
+		return NULL;
 
-   font->ptsize = ptsize;
+	font->ptsize = ptsize;
 
-   status = vgft_font_init(&font->font);
-   if (status != VCOS_SUCCESS)
-   {
-      vcos_free(font);
-      return NULL;
-   }
+	status = vgft_font_init(&font->font);
+	if (status != VCOS_SUCCESS) {
+		vcos_free(font);
+		return NULL;
+	}
+	// load the font
+	status =
+	    vgft_font_load_mem(&font->font, default_font.mem, default_font.len);
+	if (status != VCOS_SUCCESS) {
+		GX_LOG("Could not load font from memory: %d", status);
+		vgft_font_term(&font->font);
+		vcos_free(font);
+		return NULL;
+	}
 
-   // load the font
-   status = vgft_font_load_mem(&font->font, default_font.mem, default_font.len);
-   if (status != VCOS_SUCCESS)
-   {
-      GX_LOG("Could not load font from memory: %d", status);
-      vgft_font_term(&font->font);
-      vcos_free(font);
-      return NULL;
-   }
+	status = vgft_font_convert_glyphs(&font->font, ptsize, dpi_x, dpi_y);
+	if (status != VCOS_SUCCESS) {
+		GX_LOG("Could not convert font '%s' at size %d", fname, ptsize);
+		vgft_font_term(&font->font);
+		vcos_free(font);
+		return NULL;
+	}
 
-   status = vgft_font_convert_glyphs(&font->font, ptsize, dpi_x, dpi_y);
-   if (status != VCOS_SUCCESS)
-   {
-      GX_LOG("Could not convert font '%s' at size %d", fname, ptsize);
-      vgft_font_term(&font->font);
-      vcos_free(font);
-      return NULL;
-   }
+	font->next = fonts;
+	fonts = font;
 
-   font->next = fonts;
-   fonts = font;
-
-   return &font->font;
+	return &font->font;
 }
 
 void gx_font_cache_flush(void)
 {
-   while (fonts != NULL)
-   {
-      struct gx_font_cache_entry_t *next = fonts->next;
-      vgft_font_term(&fonts->font);
-      vcos_free(fonts);
-      fonts = next;
-   }
+	while (fonts != NULL) {
+		struct gx_font_cache_entry_t *next = fonts->next;
+		vgft_font_term(&fonts->font);
+		vcos_free(fonts);
+		fonts = next;
+	}
 }
 
 int32_t graphics_resource_text_dimensions_ext(GRAPHICS_RESOURCE_HANDLE res,
-                                              const char *text,
-                                              const uint32_t text_length,
-                                              uint32_t *width,
-                                              uint32_t *height,
-                                              const uint32_t text_size )
+					      const char *text,
+					      const uint32_t text_length,
+					      uint32_t * width,
+					      uint32_t * height,
+					      const uint32_t text_size)
 {
-   GX_CLIENT_STATE_T save;
-   VGfloat w, h;
-   int ret = -1;
+	GX_CLIENT_STATE_T save;
+	VGfloat w, h;
+	int ret = -1;
 
-   gx_priv_save(&save, res);
+	gx_priv_save(&save, res);
 
-   VGFT_FONT_T *font = find_font(text, text_size);
-   if (!font)
-      goto finish;
+	VGFT_FONT_T *font = find_font(text, text_size);
+	if (!font)
+		goto finish;
 
+	vgft_get_text_extents(font, text, text_length, 0.0, 0.0, &w, &h);
+	*width = w;
+	*height = h;
+	ret = 0;
 
-   vgft_get_text_extents(font, text, text_length, 0.0, 0.0, &w, &h);
-   *width = w;
-   *height = h;
-   ret = 0;
-
-finish:
-   gx_priv_restore(&save);
-   return ret;
+ finish:
+	gx_priv_restore(&save);
+	return ret;
 }
-
